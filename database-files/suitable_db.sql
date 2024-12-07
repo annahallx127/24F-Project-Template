d38e1ce89528a98278c5ba84babfd75720d33cd6
bella chin <bellachin08@gmail.com>
2024-12-07 06:55:13 -0500

    check sql file

--- database-files/suitable_db.sql
+++ database-files/suitable_db.sql
@@ -368,7 +368,7 @@ VALUES (2, 1, 'Lag', 1, 'User experienced lag when switching pages', 'Low', '202
 INSERT INTO JobListingManagement (AdminInCharge, JobID, UpdateID)
 VALUES
 (1, 1, 1),
-(1, 1, 7);
+(1, 1, 2);
 
 -- Co-op 1: Full Stack Developer
 INSERT INTO Coop (CoopID, StudentID, StartDate, EndDate, JobTitle, CompanyName)
@@ -376,8 +376,6 @@ VALUES
 (3, 2, '2024-05-01', '2024-11-01', 'Full Stack Developer', 'Tech Innovators Inc.');
 
 -- Co-op 2: Data Scientist
-INSERT INTO Coop (CoopID, StudentID, StartDate, EndDate, JobTitle, CompanyName, CoopReview, CoopRating)
+INSERT INTO Coop (CoopID, StudentID, StartDate, EndDate, JobTitle, CompanyName)
 VALUES
 (4, 2, '2025-01-15', '2025-07-15', 'Data Scientist', 'Big Data Solutions Ltd.');
-
-
